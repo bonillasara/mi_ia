1898670e7a962bb1feceb2d2b08e0efbcc6246c8
bonillasara <valent.0879000@gmail.com>
2026-05-07 10:44:46 -0500

    Update API key and reformat app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image
 # actualizacion
 
 # Pega tu API Key aquí
-api_key = "AIzaSyBNL9Fx9VffcFmjyL2Ntwtz5LS6VQHNUik"
+api_key = "AIzaSyCWmjVUFNsqtLQpzE6nDJ2jxMzOcqPdlk4"
 
 genai.configure(api_key=api_key)
 modelo = genai.GenerativeModel('gemini-2.5-flash')
